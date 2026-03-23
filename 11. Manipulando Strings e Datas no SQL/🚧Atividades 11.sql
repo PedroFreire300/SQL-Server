29321ab67425c47049a2a778f2888968fc8ebfce
Pedro Freire <freire@vipsoft.local>
2026-03-23 17:40:32 -0300

    Praticando manipulações de string + data

--- 11. Manipulando Strings e Datas no SQL/🚧Atividades 11.sql
+++ 11. Manipulando Strings e Datas no SQL/🚧Atividades 11.sql
@@ -42,7 +42,32 @@ ORDER BY LEN(ProductName) ASC
       Exercício 02
 =========================
 
+SELECT TOP (9) StyleName FROM DimProduct
 
+SELECT TOP (9) TRANSLATE (StyleName,'0123456789','ABCDEFGHIJ')
 
+AS StyleName
 
+FROM DimProduct
 
+=========================
+
+SELECT TOP (9) reverse(stuff(reverse(StyleName),2,1,'#'))
+
+FROM DimProduct
+
+=========================
+      Exercício 03
+=========================
+
+SELECT * FROM DimEmployee
+
+SELECT 
+
+CONCAT(FirstName,' ', LastName) AS 'Nome',
+EmailAddress AS 'E-mail',
+LEFT (EmailAddress, charindex('@',EmailAddress)-1) as 'ID do e-mail',
+
+CONCAT (UPPER(LEFT (EmailAddress, charindex('@',EmailAddress)-1)) , DAY(BirthDate)) AS 'Senha'
+
+FROM DimEmployee
