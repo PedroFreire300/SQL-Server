e3999e95971686af14cfa0f7de89c134d6587eee
Pedro Freire <freire@vipsoft.local>
2026-03-27 14:29:33 -0300

    Mini revisão

--- 12. Funções Condicionais/🚧Atividades 12.sql
+++ 12. Funções Condicionais/🚧Atividades 12.sql
@@ -59,6 +59,46 @@ ORDER BY Preço DESC
       Exercício 03
 =========================
 
+SELECT * FROM DimStore
 
+SELECT 
 
+Storename AS Loja,
+EmployeeCount AS Funcionário,
 
+CASE 
+
+    WHEN CAST(EmployeeCount AS Int) >= 50 THEN 'Acima de 50 funcionários'
+    WHEN CAST(EmployeeCount AS Int) >= 40 THEN 'Acima de 40 funcionários'
+    WHEN CAST(EmployeeCount AS Int) >= 30 THEN 'Acima de 30 funcionários'
+    WHEN CAST(EmployeeCount AS Int) >= 20 THEN 'Acima de 20 funcionários'
+    WHEN CAST(EmployeeCount AS Int) >= 10 THEN 'Acima de 10 funcionários'
+    ELSE 'Abaixo de 10 funcionários'
+
+END AS Categoria
+
+FROM DimStore
+
+=========================
+      Exercício 04
+=========================
+SELECT * FROM DimProduct
+SELECT * FROM DimProductSubcategory
+
+SELECT
+
+DimProductSubcategory.ProductSubcategoryName AS Nome,
+Sum(DimProduct.Weight) AS 'Peso por KG',
+
+CASE 
+
+    WHEN Sum(DimProduct.Weight) >= 1000 THEN 'Rota 2'
+    ELSE 'Rota 1'
+
+END AS Caminho
+
+FROM DimProduct
+RIGHT JOIN DimProductSubcategory
+ON DimProduct.ProductSubcategoryKey = DimProductSubcategory.ProductSubcategoryKey
+GROUP BY DimProductSubcategory.ProductSubcategoryName
+HAVING Sum(DimProduct.Weight) is NOT NULL
